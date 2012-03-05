1741ab47a49d1c1aaf4cbe04e33c9131ba725ec9
sL0ps <sl0pser@gmail.com>
2012-03-05 04:19:27 -0500

    fix in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 import os, time
-
-os.mkdir('/opt/modified/')
-time.sleep(1)
-crdir=os.getcwd()
-os.system('/bin/cp -R '+str(crdir)+' /opt/')
-time.sleep(1)
-os.system('/bin/rm /opt/modified/setup.py')
-time.sleep(1)
-os.system('/bin/cp /opt/modified/modified /etc/init.d/')
-time.sleep(1)
-os.system('/bin/rm /opt/modified/modified')
+ck=os.path.isdir('/opt/modified')
+if ck == True:
+    os.system('rm -rf /opt/modified')
+elif ck != True:
+    os.mkdir('/opt/modified/')
+    time.sleep(1)
+    crdir=os.getcwd()
+    os.system('cp -R '+str(crdir)+' /opt/')
+    time.sleep(1)
+    os.system('/bin/rm /opt/modified/setup.py')
+    time.sleep(1)
+    os.system('/bin/cp /opt/modified/modified /etc/init.d/')
+    time.sleep(1)
+    os.system('/bin/rm /opt/modified/modified')
 print "Files installed to /opt/modified"
 print "Run: /etc/init.d/modified start|status|stop|restart"
 
